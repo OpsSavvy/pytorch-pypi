8662900f9bbbf843cd177b632f138b9a831e900d
Ali Muşdu <alimusdu@gmail.com>
2025-05-14 14:30:27 +0300

    nightly added.

--- update-pytorch.sh
+++ update-pytorch.sh
@@ -102,6 +102,7 @@ EOF
 
 # update main PyTorch index, that contains everything, whatever the compute platform
 updateIndex "whl"
+updateIndex "whl/nightly"
 
 # see resulting updates
 git update-index -q --refresh
